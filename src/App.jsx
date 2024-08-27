1e47669094a6a7431e627be658a61ebc9f31d20d
Adaobilynda1234 <adaobilynda1234@gmail.com>
2024-08-27 18:39:09 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./App.css";
+
+function App() {
+  return (
+    <div className="container">
+      <h1>Hello,This is the beginning of My technical writing Repository </h1>
+    </div>
+  );
+}
+
+export default App;
